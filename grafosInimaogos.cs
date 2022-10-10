fa396210b5a6d10e8d0892365f8dbb1b0cd1ca4e
Camila Lacerda Grandini <cacagrandini@gmail.com>
2022-10-10 10:07:37 -0300

    created inital class

--- grafosInimaogos.cs
+++ grafosInimaogos.cs
@@ -1,22 +1,7 @@
-class Pessoa{
+class Aresta{
   // varíáveis privadas para o nome
   // e idade da pessoa
   private int origem;
   private int destino;
- 
-  public void setNome(string nome){
-    this.nome = nome;
-  }
-}
-public void criaNodos(Nodo origem, Nodo destino)
-{
-    nodos = new List<Nodo>();
-    nodos.Add(origem);
-    gameObjectsNodos = GameObject.FindGameObjectsWithTag("Nodo");
-    foreach (var nodo in gameObjectsNodos) {
-        Nodo aux = nodo.GetComponent<Nodo>();
-        nodos.Add(aux);
-    }
-    nodos.Add(destino);
-    grafo = new Grafo(nodos.ToArray());
+
 }
